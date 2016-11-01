ca644928b6cfaad771b19f233463c8b0862b5a90
Tyler J. Stachecki <stachecki.tyler@gmail.com>
2016-11-01 12:04:02 -0400

    libn64/os: Remove old, unused function.
    libn64 has dedicated exception handlers now and doesn't use
    an indirect branch at the exception vector anymore, so this
    function is irrelevant anyways.
    
    Signed-off-by: Tyler J. Stachecki <stachecki.tyler@gmail.com>

--- libn64/include/vr4300/cp0.h
+++ libn64/include/vr4300/cp0.h
@@ -40,21 +40,5 @@ static inline void vr4300_cp0_enable_interrupts(void) {
   );
 }
 
-// Installs an interrupt handler (at 0x80000180).
-//
-//   3C1A____  lui  k0,     0x____
-//   375A____  ori  k0, k0, 0x____
-//   03400008  jr   k0
-//   401A6800  mfc0 k0, $13
-static inline void vr4300_install_interrupt_handler(uintptr_t addr) {
-  *(volatile uint32_t *) 0xA0000180 = (0x3C1A0000) | (addr >> 16);
-  *(volatile uint32_t *) 0xA0000184 = (0x375A0000) | ((uint16_t) addr);
-  *(volatile uint32_t *) 0xA0000188 = (0x03400008);
-  *(volatile uint32_t *) 0xA000018C = (0x401A6800);
-
-  // Invalidate the instruction cache line.
-  __builtin_mips_cache(0, (void *) 0xFFFFFFFF80000180ULL);
-}
-
 #endif
 
