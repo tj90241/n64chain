f7282a89e2562093d8068c2dcc0322bfb9b9f1c6
Jason Benaim <jkbenaim@gmail.com>
2022-09-27 00:20:46 -0700

    Update GNU make from version 4.2.1 to 4.3, allowing us to stop shipping a patch.

--- tools/build-win64-toolchain.sh
+++ tools/build-win64-toolchain.sh
@@ -14,7 +14,7 @@ set -eu
 BINUTILS="ftp://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.bz2"
 GCC="ftp://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.gz"
 GMP="ftp://ftp.gnu.org/gnu/gmp/gmp-6.2.0.tar.bz2"
-MAKE="ftp://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
+MAKE="ftp://ftp.gnu.org/gnu/make/make-4.3.tar.gz"
 MPC="ftp://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz"
 MPFR="ftp://ftp.gnu.org/gnu/mpfr/mpfr-4.0.2.tar.bz2"
 
